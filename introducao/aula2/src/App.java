ab48fe995fb3e95d829417eddf1d397aaaf6c2f4
brvale05 <brvale05@gmail.com>
2025-11-05 17:49:22 -0300

    update

--- introducao/aula2/src/App.java
+++ introducao/aula2/src/App.java
@@ -8,5 +8,14 @@ public class App
         System.out.println("Cidade: " + end.getCidade());
         System.out.println("Estado: " + end.getEstado());
         System.out.println("CEP: " + end.getCep());
+
+        Cliente client = new Cliente("bruno vale", "18871887632", "27 992049698");
+
+        System.out.println("NOME: " + client.getNome());
+        System.out.println("CPF: " + client.getCpf());
+        System.out.println("Telefone: " + client.getTelefone());
+
+        client.setEndereco(end);
+
     }
 }
